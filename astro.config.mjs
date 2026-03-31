5ecbdf914fea3734828ecfcc0820ae3d7ae82828
Rifqy <mamedrifqy@yahoo.com>
2026-03-31 18:59:18 +0700

    add github actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import mdx from '@astrojs/mdx';
 export default defineConfig({
   integrations: [react(), mdx()],
   output: 'static',
-  // If deploying to GitHub Pages, set your repo name:
-  // base: '/riau-gis-blog',
+  site: 'https://mamedrifqy.github.io',
+  base: '/blog',
 });
